a69e18e51cc3561584369ea74cbe02475f44ab3d
SzymonAdamski <szymon.adamski@microsoft.wsei.edu.pl>
2024-10-21 21:56:45 +0200

    zrobione book przed zajęciami
    idk czy dobrze jutro zobaczymy

---
+++ WebApp/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(Book model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success", model);
+            }
+            else
+            {
+                return View("Form", model);
+            }
+        }
+    }
+}
---
+++ WebApp/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models
+{
+    public class Book
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać tytuł!")]
+        [StringLength(100, ErrorMessage = "Tytuł nie może przekraczać 100 znaków!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać autora!")]
+        [StringLength(50, ErrorMessage = "Autor nie może przekraczać 50 znaków!")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać liczbę stron!")]
+        [Range(1, int.MaxValue, ErrorMessage = "Liczba stron musi być dodatnia!")]
+        public int Pages { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać numer ISBN!")]
+        [RegularExpression(@"\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d{1}", ErrorMessage = "Podaj poprawny numer ISBN!")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać rok wydania!")]
+        [Range(1450, 2100, ErrorMessage = "Rok wydania musi być w zakresie od 1450 do 2100!")]
+        public int YearPublished { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać wydawnictwo!")]
+        [StringLength(50, ErrorMessage = "Nazwa wydawnictwa nie może przekraczać 50 znaków!")]
+        public string Publisher { get; set; }
+    }
+}
