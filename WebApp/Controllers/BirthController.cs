07112c0ecfed478f586e4eb1fbd8ef98d76b3fb7
SzymonAdamski <szymon.adamski@microsoft.wsei.edu.pl>
2024-10-21 21:48:30 +0200

    zad 2
    dodano metodę i kontroler dla obliczania wieku

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BirthController : Controller
+    {
+        // Wyświetla formularz
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result([FromForm] Birth model)
+        {
+            if (!model.IsValid())
+            {
+                ViewBag.ErrorMessage = "Niepoprawne dane. Upewnij się, że imię, nazwisko zostały podane, a data urodzenia jest wcześniejsza od daty bieżącej.";
+                return View("CustomError");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ WebApp/Models/Birth.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApp.Models
+{
+    public class Birth
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; } // Nowa właściwość
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name) &&
+                   !string.IsNullOrWhiteSpace(Surname) &&
+                   BirthDate.HasValue &&
+                   BirthDate.Value < DateTime.Now;
+        }
+
+        public int CalculateAge()
+        {
+            if (!BirthDate.HasValue)
+                throw new InvalidOperationException("Birth date is not set.");
+
+            var today = DateTime.Today;
+            var age = today.Year - BirthDate.Value.Year;
+            if (BirthDate.Value.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
